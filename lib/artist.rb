a9b95a5537e524dd1e077c94846d9d76f1aa8aa8
David Cantoran <davidcantoran36@gmail.com>
2019-12-17 22:00:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,15 @@
 class Artist 
+  attr_accessor :name 
+  def initialize(name)
+    @name = name 
+  end 
   
-  def initialize
-    
+  def songs 
+    Song.all.name 
+  end 
+  
+  def add_song(song)
+    song.artist = self 
   end 
   
 end 
